357b3d50f6cae901afb07591f5dce371d8d21d49
Saqaf <saqafaliabdo716@gmail.com>
2025-08-10 07:40:54 +0300

    fix/update launguge style

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,62 +7,76 @@ manueNav.addEventListener("click", () => {
 });
 
 // loginForm
-
-let email = document.getElementById("email");
-let emailValid =false;
- email.addEventListener("input", function () {
-  // input evnt operation in real time
-  if (email.value !== "" && email.value.length > 7 && email.value.length < 14) {
-    email.style.outline = `solid 2px green `;
-    document.getElementById("emailError").textContent = "";
-    emailValid = true;
-  } else {
-    email.style.outline = "solid 2px red";
-    document.getElementById("emailError").textContent =
-      "Must be between 7 and 14 characters.";
-    // emailValid = false;
-  }
-});
-
-
-let password=document.getElementById('password');
-let passwordValid = false;
- password.addEventListener("input", function () {
-  // input evnt operation in real time
-  if (password.value !== "" && password.value.length > 5 && password.value.length < 15) {
-    password.style.outline = `solid 2px green `;
-    document.getElementById("passwordError").textContent = "";
-    passwordValid = true;
-  } else {
-    password.style.outline = "solid 2px red";
-    document.getElementById("passwordError").textContent =
-      "Must be between 5 and 14 characters.";
-    // return false;
-  }
-});
-
-console.log(emailValid);
-console.log(passwordValid);
-// login button
 const loginButton = document.getElementById("loginButton");
-loginButton.addEventListener("click", function (e) {
-console.log(emailValid);
-    // e.preventDefault();
-  if (emailValid==true && passwordValid==true) {
-    
-    localStorage.setItem("email", email.value);
-    localStorage.setItem("password", password.value);
-    // Redirect to the home page
-    e.preventDefault();
-    console.log("Login successful");
-    window.location.href = "index.html";
+if (loginButton) {
+  let email = document.getElementById("email");
+  let emailValid = false;
+  email.addEventListener("input", function () {
+    // input evnt operation in real time
+    if (email.value !== "" && email.value.length > 7 && email.value.length < 14) {
+      email.style.outline = `solid 2px green `;
+      document.getElementById("emailError").textContent = "";
+      emailValid = true;
+    } else {
+      email.style.outline = "solid 2px red";
+      document.getElementById("emailError").textContent =
+        "Must be between 7 and 14 characters.";
+      // emailValid = false;
+    }
+  });
 
- return;
+
+  let password = document.getElementById('password');
+  let passwordValid = false;
+  password.addEventListener("input", function () {
+    // input evnt operation in real time
+    if (password.value !== "" && password.value.length > 5 && password.value.length < 15) {
+      password.style.outline = `solid 2px green `;
+      document.getElementById("passwordError").textContent = "";
+      passwordValid = true;
+    } else {
+      password.style.outline = "solid 2px red";
+      document.getElementById("passwordError").textContent =
+        "Must be between 5 and 14 characters.";
+      // return false;
+    }
+  });
+
+  loginButton.addEventListener("click", function (e) {
+    if (emailValid == true && passwordValid == true) {
+      localStorage.setItem("email", email.value);
+      localStorage.setItem("password", password.value);
+      // Redirect to the home page
+      e.preventDefault();
+      window.location.href = "index.html";
+
+      return;
+    } else {
+      e.preventDefault();
+    }
+  });
+}
+
+const themeToggle = document.getElementById("theme-toggle");
+const body = document.body;
+
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme) {
+  body.classList.add(currentTheme);
+  if (currentTheme === "dark-mode") {
+    themeToggle.classList.replace("fa-moon", "fa-sun");
+  }
+}
+
+themeToggle.addEventListener("click", () => {
+  if (body.classList.contains("dark-mode")) {
+    body.classList.remove("dark-mode");
+    localStorage.setItem("theme", "light");
+    themeToggle.classList.replace("fa-sun", "fa-moon");
   } else {
-    e.preventDefault();
-    console.log("Login failed");
-    
+    body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark-mode");
+    themeToggle.classList.replace("fa-moon", "fa-sun");
   }
 });
-
-
